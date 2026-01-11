971530f289389a8a8171aa9f5fdeba5def8c5279
email <ssudhar415@gmail.com>
2026-01-11 23:10:54 +0530

    use firebase credentials from env vars

--- E-Commerce-Server/config/firebase.config.js
+++ E-Commerce-Server/config/firebase.config.js
@@ -1,9 +1,14 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../cred/servicekey.json");
+const admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
-console.log("🔥 Firebase initialized");
 
+const db = admin.firestore();
+const auth = admin.auth();
+
+module.exports = { db, auth };
